e34e4cb5459de9fbf53c1704c405b35cf70948a9
Jackson Mitchusson <jackson.b.mitchusson@ou.edu>
2023-02-20 22:58:11 -0600

    Added user input

--- assignment.py
+++ assignment.py
@@ -2,11 +2,24 @@
 def sumList(arr):
     total = 0
     for i in arr:
-        total += i;
+        total += i
     return total
 
 def multList(arr):
     total = 1
     for i in arr:
-        total *= i;
+        total *= i
     return total
+
+userInput = ""
+list = []
+while(userInput != "x"): 
+    userInput = input("Enter numbers until you are done, then enter 'x' >")
+    if userInput.isnumeric():
+        list.append(int(userInput))
+
+print("Input: ", end='')
+for i in list:
+    print(str(i) + " ", end='')
+print("\nSum of list: %d" % (sumList(list)))
+print("Product of list: %d" % (multList(list)))
