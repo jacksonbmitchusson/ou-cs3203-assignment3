2468100c82baf7b6ebacabb6cda3cb12f6253b18
Jackson Mitchusson <jackson.b.mitchusson@ou.edu>
2023-02-20 22:03:52 -0600

    Added sum function

--- assignment.py
+++ assignment.py
@@ -1,6 +1,12 @@
 
-def sum(arr):
+def sumList(arr):
     total = 0
     for i in arr:
         total += i;
+    return total
+
+def multList(arr):
+    total = 1
+    for i in arr:
+        total *= i;
     return total
